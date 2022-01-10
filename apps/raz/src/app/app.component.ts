59e6e0bc4656fb4af1dec02f319c25aa57d34a00
razgaon-centri <raz.gaon@centrical.com>
2022-01-10 09:33:24 -0700

    initial commit

---
+++ apps/raz/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'raz-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'raz';
+}
